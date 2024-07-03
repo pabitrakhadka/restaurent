29c2c5e433befef4aed4c5bcf8ee00a1742b5513
pabitra <pabitrakhadka424@gmail.com>
2024-07-03 23:28:21 +0545

    update3

--- src/pages/api/validate/iswebsite.js
+++ src/pages/api/validate/iswebsite.js
@@ -1,7 +1,6 @@
 // Check if the request has cookies || web site request or not
 async function isWebsite(request) {
     const cookies = request.cookies || null;
-
     if (cookies && Object.keys(cookies).length > 0) {
         return true;
     }
