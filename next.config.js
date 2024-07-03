dc2f98f1c55b6d0a5a4094611e730e7236a7a0e5
Pabitra Khadka <104818221+pabitrakhadka@users.noreply.github.com>
2024-07-04 05:12:40 +0545

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,31 +5,7 @@ const nextConfig = {
     bodyParser: {
       sizeLimit: '10mb',
     },
-  },
-  images: {
-    domains: [
-      'media.istockphoto.com',
-      'www.shutterstock.com',
-      'img.freepik.com',
-      'www.google.com',
-      'cdn-icons-png.flaticon.com',
-      'www.seekpng.com',
-    ],
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // These modules are server-side only, set them to false in webpack for the client-side
-      config.resolve.fallback = {
-        fs: false,
-        net: false,
-        dns: false,
-        child_process: false,
-        tls: false,
-      };
-    }
+  }
+}
 
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
