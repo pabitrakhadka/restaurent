bd145a4cc22dd9ea47a60ab91741cc96c3e77952
pabitra <pabitrakhadka424@gmail.com>
2024-07-03 16:58:06 +0545

    nextcongig changeess

--- next.config.js
+++ next.config.js
@@ -1,37 +1,49 @@
-const path = require('path');
+// const path = require('path');
 
+// const nextConfig = {
+//   images: {
+//     domains: [
+//       'media.istockphoto.com',
+//       'www.shutterstock.com',
+//       'img.freepik.com',
+//       'www.google.com',
+//       'cdn-icons-png.flaticon.com',
+//       'www.seekpng.com',
+//     ],
+//   },
+//   reactStrictMode: true,
+//   serverRuntimeConfig: {
+//     // Will only be available on the server side
+//     bodyParser: {
+//       sizeLimit: '10mb',
+//     },
+//   },
+//   webpack: (config, { isServer }) => {
+//     if (!isServer) {
+//       // These modules are server-side only, set them to false in webpack for the client-side
+//       config.resolve.fallback = {
+//         fs: false,
+//         net: false,
+//         dns: false,
+//         child_process: false,
+//         tls: false,
+//       };
+//     }
+
+//     return config;
+//   },
+// };
+
+// module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      'media.istockphoto.com',
-      'www.shutterstock.com',
-      'img.freepik.com',
-      'www.google.com',
-      'cdn-icons-png.flaticon.com',
-      'www.seekpng.com',
-    ],
-  },
   reactStrictMode: true,
   serverRuntimeConfig: {
     // Will only be available on the server side
     bodyParser: {
       sizeLimit: '10mb',
     },
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // These modules are server-side only, set them to false in webpack for the client-side
-      config.resolve.fallback = {
-        fs: false,
-        net: false,
-        dns: false,
-        child_process: false,
-        tls: false,
-      };
-    }
+  }
+}
 
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
